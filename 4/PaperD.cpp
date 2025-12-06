f2f6528fe9c4056691c524e6a423ab53f58ca6eb
Xabier <xortega@gmail.com>
2025-12-06 19:10:09 +0100

    day 4 visualicer and day 6 done

--- 4/PaperD.cpp
+++ 4/PaperD.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 //BLUE -> GREEN -> YELLOW -> RED
 /*
@@ -117,13 +119,11 @@ std::string *simulate(std::string *matrix){
         {
             if (matrix[y][x] == '.')
             {
-                std::cout << '.';
                 new_matrix[y][x] = '.';
                 continue;
             }
             if (matrix[y][x] == 'x')
             {
-                std::cout << '.';
                 new_matrix[y][x] = '.';
                 continue;
             }
@@ -153,35 +153,66 @@ std::string *simulate(std::string *matrix){
             if (arround < 4)
             {
                 heatmap[(y + HEIGHT) % HEIGHT][x] = ANSI_32[gen % 65 + 1];
-                std::cout << heatmap[(y + HEIGHT) % HEIGHT][x] << 'x' << ANSI_RESET;
                 new_matrix[(y + HEIGHT) % HEIGHT][x] = 'x';
             }
             else
-            {
-                std::cout << '@';
                 new_matrix[(y + HEIGHT) % HEIGHT][x] = '@';
-            }
 
         }
-        std::cout << std::endl;
     }
     return (new_matrix);
 }
 
-int main(void) {
+int main(int argc, char **argv)
+{
 
     std::string line;
     std::ifstream input("xabi.txt");
     std::string *matrix = new std::string[HEIGHT];
     std::string *new_matrix = new std::string[HEIGHT];
+    
+    std::vector<std::string> arguments;
+    if (argc > 1)
+        arguments.assign(argv + 1, argv + argc);
+
+    bool option_color = (std::find(arguments.begin(), arguments.end(), "-c") != arguments.end());
+    
+    std::string option_char = "@";
+
+    if (std::find(arguments.begin(), arguments.end(), "-ch") != arguments.end())
+    {
+        if (std::find(arguments.begin(), arguments.end(), "-ch") + 1 == arguments.end())
+            ;
+        else
+            option_char = *(std::find(arguments.begin(), arguments.end(), "-ch") + 1);
+        if (option_char.size() > 1 && option_char[0] == '-')
+            option_char = "@";
+    }
+
+
+    int option_size = 1;
+
+    if (std::find(arguments.begin(), arguments.end(), "-s") != arguments.end())
+    {
+        if (std::find(arguments.begin(), arguments.end(), "-s") + 1 == arguments.end())
+            ;
+        else
+            option_size = std::atoi((std::find(arguments.begin(), arguments.end(), "-s") + 1)->c_str());
+        option_size += (option_size == 0);
+    }
+    std::cout << "color : " << (option_color ? "true" : "false") << std::endl;
+    std::cout << "char : " << option_char << std::endl;
+    std::cout << "size : " << option_size << std::endl;
+
     int i = 0;
     while (input.is_open() && std::getline(input, line)) {
-        matrix[i] = line;
-        i++;
+        matrix[i++] = line;
     }
-    for (size_t i = 0; i < HEIGHT; i++)
+
+/*
+         for (size_t i = 0; i < HEIGHT; i++)
         std::cout << matrix[i] << std::endl;
-    
+ */    
     int sum = countPaper(matrix);
     
 
@@ -201,8 +232,30 @@ int main(void) {
     }
     std::cout << sum - countPaper(new_matrix) << std::endl;
     std::cout << gen << std::endl;
+    {
+    if (option_color)
+        for (size_t tile_y = 0; tile_y < option_size; tile_y++)
+        {
+            for (size_t y = 0; y < HEIGHT; y++)
+            {
+                for (size_t tile_x = 0; tile_x < option_size; tile_x++)
+                   for (size_t x = 0; x < WIDTH; x++)
+                        std::cout << heatmap[y][x] << "█" << ANSI_RESET;
+                std::cout << std::endl;
+            }
+        }
+    }
+    else
+        for (size_t i = 0; i < HEIGHT; i++)
+           std::cout << new_matrix[i] << std::endl;
+    std::cout << "\033[0m\n" << std::endl;
+    
+    return(0);
+    
     for (size_t y = 0; y < HEIGHT; y++)
     {
+        for (size_t x = 0; x < WIDTH; x++)
+           std::cout << heatmap[y][x] << "█" << ANSI_RESET;
         for (size_t x = 0; x < WIDTH; x++)
            std::cout << heatmap[y][x] << "█" << ANSI_RESET;
         std::cout << std::endl;
@@ -210,7 +263,9 @@ int main(void) {
     for (size_t y = 0; y < HEIGHT; y++)
     {
         for (size_t x = 0; x < WIDTH; x++)
-           std::cout << heatmap[y][x] << "@" << ANSI_RESET;
+           std::cout << heatmap[y][x] << "█" << ANSI_RESET;
+        for (size_t x = 0; x < WIDTH; x++)
+            std::cout << heatmap[y][x] << "█" << ANSI_RESET;
         std::cout << std::endl;
     }
     for (int i = 1; i < 65; ++i) {
@@ -219,8 +274,7 @@ int main(void) {
 
     std::cout << "\033[38;5;15m" << std::endl;
 
-    for (size_t i = 0; i < HEIGHT; i++)
-        std::cout << new_matrix[i] << std::endl;
+
     std::cout << "\033[0m\n" << std::endl;
     return(0);
 }
--- 6/MathB.cpp
+++ 6/MathB.cpp
@@ -3,123 +3,43 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
-
-long sumAll(std::string first, std::string second, std::string third, std::string fourth)
-{
-    long sum = 0;
-    while (!first.empty() || !second.empty() || !third.empty() || !fourth.empty())
-    {
-        std::string tmp;
-        if (!first.empty())
-        {
-            tmp.push_back(first.front());
-            first.erase(0, 1);
-        }
-        if (!second.empty())
-        {
-            tmp.push_back(second.front());
-            second.erase(0, 1);
-        }
-        if (!third.empty())
-        {
-            tmp.push_back(third.front());
-            third.erase(0, 1);
-        }
-        if (!fourth.empty())
-        {
-            tmp.push_back(fourth.front());
-            fourth.erase(0, 1);
-        }
-        sum += std::atol(tmp.c_str());
-    }
-    return sum;
-}
-
-
-long multAll(std::string first, std::string second, std::string third, std::string fourth)
-{
-    long sum = 1;
-    int longest = std::max(first.size(), second.size(), third.size(), fourth.size())
-    while (!first.empty() || !second.empty() || !third.empty() || !fourth.empty())
-    {
-        std::string tmp;
-        if (!first.empty())
-        {
-            tmp.push_back(first.front());
-            first.erase(0, 1);
-        }
-        if (!second.empty())
-        {
-            tmp.push_back(second.front());
-            second.erase(0, 1);
-        }
-        if (!third.empty())
-        {
-            tmp.push_back(third.front());
-            third.erase(0, 1);
-        }
-        if (!fourth.empty())
-        {
-            tmp.push_back(fourth.front());
-            fourth.erase(0, 1);
-        }
-        std::cout << tmp << " * ";
-        sum *= std::atol(tmp.c_str());
-    }
-    std::cout << " = " << sum << std::endl;
-
-    return sum;
-}
-
-
+#include <numeric>
 
 int main(int argc, char const *argv[])
 {
     std::string line;
     std::ifstream input("conbination.txt");
     long sum = 0;
-    std::stringstream tmp;
-    std::vector<std::vector<std::string> > nums;
+    std::vector<std::string > matrix;
     std::string operators;
+    std::vector<long> buffer;
 
-    while (std::getline(input, line))
+    while (std::getline(input, line) && !(line[0] == '+' || line[0] == '*'))
+        matrix.push_back(line);
+
+        operators = line;
+
+    for (size_t i = operators.size(); i > 0; --i)
     {
-        if(line[0] == '+' || line[0] == '*')
-            break;
-        std::stringstream tmp( line );
-
-        std::string number;
-        std::vector<std::string> newNums;
-        while ( tmp >> number )
-            newNums.push_back( number );
-        nums.push_back(newNums);
+        long tmp = 0;
+        if (matrix[0][i - 1] != ' ') tmp = tmp * 10 + (matrix[0][i - 1] - 48);
+        if (matrix[1][i - 1] != ' ') tmp = tmp * 10 + (matrix[1][i - 1] - 48);
+        if (matrix[2][i - 1] != ' ') tmp = tmp * 10 + (matrix[2][i - 1] - 48);
+        if (matrix[3][i - 1] != ' ') tmp = tmp * 10 + (matrix[3][i - 1] - 48);
         
-    }
-    operators = line;
-    operators.erase(std::remove(operators.begin(), operators.end(), ' '), operators.end());
+        if (tmp != 0) buffer.push_back(tmp);
 
-/*     for (std::vector<long> &vec : nums)
-    {
-        for (long i : vec)
+        if (operators[i - 1] == '+')
         {
-            std::cout << i << " ";
+            sum += std::accumulate(buffer.begin(), buffer.end(), long(0));
+            buffer.clear();
         }
-        std::cout << std::endl;
+        if (operators[i - 1] == '*')
+        {
+            sum += std::accumulate(buffer.begin(), buffer.end(), 1L, std::multiplies<long>());
+            buffer.clear();
+        }
     }
-    for (char c : operators)
-    {
-        std::cout << c << " ";
-    }
-    std::cout << std::endl; */
-    
-    for (size_t i = 0; i < operators.size(); i++)
-    {
-        if (operators[i] == '+')
-            sum += sumAll(nums[0][i], nums[1][i], nums[2][i], std::string(""));
-        if (operators[i] == '*')
-            sum += multAll(nums[0][i], nums[1][i], nums[2][i], std::string(""));
-    }
-    
     std::cout << sum << std::endl;
     return 0;
 }
